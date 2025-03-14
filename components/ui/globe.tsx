0f80f119d0a2b073ba3fd4ec5c18a0174c2c2e0d
anitdias <anitdias3@gmail.com>
2025-03-14 16:36:19 +0530

    ui-mod-3

--- components/ui/globe.tsx
+++ components/ui/globe.tsx
@@ -1,14 +1,14 @@
 "use client"
-import { useEffect, useRef, useState } from "react"
+import { JSX, useEffect, useRef, useState } from "react"
 import { Color, Scene, Fog, PerspectiveCamera, Vector3 } from "three"
 import ThreeGlobe from "three-globe"
-import { useThree, Object3DNode, Canvas, extend } from "@react-three/fiber"
+import { useThree, Canvas, extend } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
 import countries from "@/data/globe.json"
 
 declare module "@react-three/fiber" {
   interface ThreeElements {
-    threeGlobe: Object3DNode<ThreeGlobe, typeof ThreeGlobe>
+    threeGlobe: { ref?: React.Ref<ThreeGlobe> } & JSX.IntrinsicElements["group"]
   }
 }
 
